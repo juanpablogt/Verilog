fc649a645303237221cfe0bd9110038a6d0d9603
JPablo <juapabgonzalez@utp.edu.co>
2023-07-05 21:48:21 -0500

    cambios

--- behivoral.v
+++ behivoral.v
@@ -1,16 +1,12 @@
-module full_adder_behivoral(
-    input a,
-    input b,
-    input cin,
-    output cout,
-    output sum
-    );
-    wire w1,w2,w3;
-    xor x1(sum,a,b);
-    xor x2(cout,a,b);
-    and a1(w1,a,b);
-    and a2(w2,a,cin);
-    and a3(w3,b,cin);
-    or o1(cout,w1,w2,w3);
-    
+module full_adder_behivoral(s,c,a,b);
+
+    input a,b;
+    output s,c;
+    always @(a,b)
+
+    begin
+        s = a ^ b;
+        c = a & b;
+    end
+
 endmodule
