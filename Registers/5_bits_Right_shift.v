bf5846e3648002570c6818b6b110a10a4ea6c7fc
JPablo <juapabgonzalez@utp.edu.co>
2023-08-01 10:06:20 -0500

    Register

---
+++ Registers/5_bits_Right_shift.v
@@ -0,0 +1,12 @@
+module SR_LR(SO,clk,rst,SI);
+input SI,clk,rst;
+output SO;
+reg [4:0] SR;
+
+always@(posedge clk, negedge rst)
+    if(!rst)
+        SR <= 5'd0;
+    else
+        SR <= {SR[3:0],SI};
+assing SO = SR[4];
+endmodule
