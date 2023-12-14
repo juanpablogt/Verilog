2506170cf90f9cb62068b74be07056926ec85d49
JPablo <juapabgonzalez@utp.edu.co>
2023-12-13 23:56:18 -0500

    Retomando_verilog

---
+++ simple/simple_tb.sv
@@ -0,0 +1,35 @@
+module circ_simple;
+    reg A;
+    reg B;
+    reg C;
+    wire X;
+    wire Y;
+
+    circuito_simple cir1(.A(A), .B(B), .C(C), .X(X), .Y(Y));
+
+    initial begin
+      $dumpfile("circ_simple.vcd");
+      $dumpvars(0, circ_simple);
+
+      A = 1'b0; B = 1'b0; C = 1'b0;
+      #10;
+      A = 1'b0; B = 1'b0; C = 1'b1;
+      #10;
+      A = 1'b0; B = 1'b1; C = 1'b0;
+      #10;
+      A = 1'b0; B = 1'b1; C = 1'b1;
+      #10;
+      A = 1'b1; B = 1'b0; C = 1'b0;
+      #10;
+      A = 1'b1; B = 1'b0; C = 1'b1;
+      #10;
+      A = 1'b1; B = 1'b1; C = 1'b0;
+      #10;
+      A = 1'b1; B = 1'b1; C = 1'b1;
+      #10;
+      $finish;
+    end
+
+endmodule
+
+  
