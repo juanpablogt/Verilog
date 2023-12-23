21278aa9c043f16f77ae6eb83b58c7edcc1efb21
JPablo <juapabgonzalez@utp.edu.co>
2023-12-23 11:56:42 -0500

    full_addre

--- helf_adder/helf_adder.sv
+++ helf_adder/helf_adder.sv
@@ -1,7 +1,7 @@
-module helf_adder(
-    input a, b,
-    output sum, carry
-);
+module helf_adder(a,b,sum,carry);
+    input a, b;
+    output sum, carry;
+
     // Ecuación lógica
     assign sum = a ^ b;
     assign carry = a & b;
